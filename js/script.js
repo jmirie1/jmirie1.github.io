622cf2f3ffb3aa1f9bcd2034793bf59a4dea1625
jmirie1 <juan.irie_1@mail.udp.cl>
2024-05-25 20:03:10 -0400

    archives work responsiveness

--- js/script.js
+++ js/script.js
@@ -4,3 +4,28 @@ $gallery.masonry({
   columnWidth: 200, // adjust the column width
   gutter: 20 // adjust the gap between columns
 });
+
+
+
+
+const categories = document.querySelectorAll('.categories li a');
+const images = document.querySelectorAll('.image');
+
+categories.forEach((category) => {
+  category.addEventListener('click', (e) => {
+    e.preventDefault();
+    categories.forEach((c) => c.classList.remove('active'));
+    e.target.classList.add('active');
+    const categoryName = e.target.getAttribute('data-category');
+    filterSelection(categoryName);
+  });
+});
+
+function filterSelection(c) {
+  images.forEach((image) => {
+    image.classList.remove('show');
+    if (image.classList.contains(c)) {
+      image.classList.add('show');
+    }
+  });
+}
